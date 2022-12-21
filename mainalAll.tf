883d19f840c37560d0e22afcfcebb27ffe262704
MahaMassalha <96788273+MahaMassalha@users.noreply.github.com>
2022-12-21 15:55:58 +0200

    update

--- mainalAll.tf
+++ mainalAll.tf
@@ -1,13 +1,47 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+module "WebServers" {
+    source = "./WebServers/"
+    AmiName= "ami-0caef02b518350c8b"
+    InstenceType= "t2.micro"
+    Zone = "eu-central-1"
+    key = "maha"
+
+}
+
+module "LoadBalancers" {
+    source = "./loadBalancers/"
+    nameofiternal = "internalLB"
+    nameofinternet = "externalLB" 
+}
+
+module "ApplicationServer" {
+    source = "./ApplicationServer/"
+    at = "t2.micro"
+    c = 4
+}
+
+
+
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+
+  tags = {
+    Name        = "mybucket"
   }
 }
 
-//in which region we want to work
-provider "aws" {
-  region = "eu-central-1"
+resource "aws_db_instance" "myrdsdb" {
+  identifier = "mysqldatabase"
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  username             = "admin"
+  password             = "password" 
+  instance_class       = "db.t2.micro"
+  storage_type         = "gp2"
+  parameter_group_name = "default.mysql5.7"
+  port                 = 3306
+  skip_final_snapshot  = true
+  multi_az = true
 }
